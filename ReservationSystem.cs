b59a6a4311de04209b17a92e6bcdb8a0855822e6
Will <Dogguy99@gmail.com>
2023-05-07 15:40:10 -0500

    ReservationSystem Comments

--- ReservationSystem.cs
+++ ReservationSystem.cs
@@ -11,10 +11,12 @@ namespace ConsoleApp7
     internal class ReservationSystem
     {
         List<ReservationDay> days = new List<ReservationDay>();
+        //List for creating and storing individual reservation days
         public void AddReservationDay(int monthNumber, int dayNumber)
         {
             days.Add(new ReservationDay(monthNumber, dayNumber));
         }
+        //Void method for adding a resevation day to the list. Uses the monthNumber and dayNumber to identify which days are open
         public void DisplayDaysOfOperation()
         {
             int totalDays = 1;
@@ -23,17 +25,22 @@ namespace ConsoleApp7
                 Console.WriteLine($"#{totalDays}: {i.MonthNumber}/{i.DayNumber}");
                 totalDays += 1;
             }
+           //foreach loop that counts each day of each month that is open
         }
+        //Void Method that displays the total days of operation
         public void RemoveReservationDay()
         {
           Console.WriteLine("Enter the Month of the Date to be removed");
           int month = Convert.ToInt32(Console.ReadLine());
+            //Uses user input to determine which month has the day that needs to be removed
           Console.WriteLine("Enter the Day of the Date to be removed");
           int day = Convert.ToInt32(Console.ReadLine());
+            //Uses user input to determine the day in the month that needs to be removed
           int Index =  days.FindIndex(x => x.MonthNumber == month && x.DayNumber == day);
           days.RemoveAt(Index);
+            //Finds and then removes the previosly specified date
           Console.WriteLine("Entry has been deleted");
         }
-
+        //Void method that prompts the user with questions in order to removve a reservation day
     }
 }
