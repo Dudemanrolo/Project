73397c0e241f81587120b9d8b62c607f25b75101
Will <Dogguy99@gmail.com>
2023-05-09 17:24:56 -0500

    Comments fot passwordSystem

--- passwordSystem.cs
+++ passwordSystem.cs
@@ -10,11 +10,14 @@ namespace ConsoleApp7
     {
         private List<string> username = new List<string>();
         private List<string> password = new List<string>();
+        //Lists to store all of teh usernames and passwords in the system
         public passwordSystem(string username, string password)
         {
             this.username.Add(username);
             this.password.Add(password);
         }
+        //Object for creating a new password
+
         public void SignIn()
         {
             bool Authenticate = false;
@@ -24,9 +27,11 @@ namespace ConsoleApp7
                 {
                     Console.WriteLine("Enter Your User Name:");
                     string username = Console.ReadLine();
+                    //Asks the user for their username and then sets it to username
                     Console.Clear();
                     Console.WriteLine("Enter Your Password:");
                     string password = Console.ReadLine();
+                    //Asks the user for their password and then sets it to password
                     Console.Clear();
                     if (this.username.Exists(x => x == username))
                     {
@@ -35,29 +40,39 @@ namespace ConsoleApp7
                         {
                             Authenticate = true;
                         }
+                        //If both the userrname and password entered match that of the database, then the user is authenticated
                         else
                         {
                             Console.WriteLine("Your Password or Username is Incorrect");
                         }
+                        //else in case if the user has a valid username but the wrong password
                     }
+                    //if statement to determine if the username and password are in the system
                     else
                     {
                         Console.WriteLine("Your Password or Username is Incorrect");
                     }
-                } catch ( Exception e)
+                    //else to tell the user that their information is wrong
+                }
+                catch (Exception e)
                 {
                     Console.WriteLine("An Error Has Occured, Please Try Again.");
                 }
+                //catch for any non valid inputs
             }
         }
+        //Void method for SignIn
+        //User must sign in in order to access and other program functions0
         public void addUser()
         {
             try
             {
                 Console.WriteLine("Enter Your Desired UserName:");
                 string username = Console.ReadLine();
+                //Asks the user for desired username and then sets it to username
                 Console.WriteLine("Enter Your Desired Password:");
                 string password = Console.ReadLine();
+                //Asks the user for desired password and then sets it to password
                 if (this.username.Contains(username) != true)
                 {
                     this.username.Add(username);
@@ -65,11 +80,15 @@ namespace ConsoleApp7
                     Console.WriteLine("User successfully Added...returning to previous menu");
 
                 }
-            } catch (Exception e)
+                //Creates a new username and password as long as the username is not already in the system
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("An Error Has Occured, Please Try Again");
             }
+            //catch for any non valid inputs
         }
+        //Void method for adding a user to the system
 
     }
 }
